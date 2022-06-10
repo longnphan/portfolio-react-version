46a8e8bb8eefe95c8e7330184c086e66917d28ab
Long Phan <longnphan@outlook.com>
2022-06-09 21:34:51 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer/footer.component";
+import Navbar from "./components/Navbar/navbar.component";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    );
+};
+
+export default App;
